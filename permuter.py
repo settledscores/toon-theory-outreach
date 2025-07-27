3c38b9571fd9037dec91d56d3e297cc4d457b02c
settledscores <deresistance402@gmail.com>
2025-07-27 09:29:05 +0100

    Update permuter.py

--- permuter.py
+++ permuter.py
@@ -4,7 +4,6 @@ from urllib.parse import urlparse
 
 INPUT_PATH = "leads/scraped_leads.ndjson"
 PERMS_PATH = "leads/permutations.txt"
-EMAILS_PATH = "leads/emails.txt"
 
 def extract_domain(url):
     try:
